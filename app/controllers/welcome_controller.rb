38d9d8560be4ef295bbd91a440a305063b395ebb
Jeremy Gunter <gunterja@gmail.com>
2016-03-17 20:05:51 -0500

    Add a bunch of stuff. Pictures. Lists. Change some styling. A whole boatload of shit.

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,14 @@
+class WelcomeController < ApplicationController
+  def index
+    @countries = ["Peru", "Bolivia", "Norway"]
+  end
+
+  def about
+  end
+
+  def photos
+  end
+
+  def another_page
+  end
+end
